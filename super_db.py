2a80e9b39992250c9483678a722b40ad3c0601e9
Daniel <d.a.p.mendez@outlook.com>
2024-10-20 23:21:04 -0600

    Modifica super_db

---
+++ super_db.py
@@ -0,0 +1,17 @@
+usuarios = {
+    1: {
+        "nombre": "Pepe Toño",
+        "apellido": "Lopez Perez",
+        "edad": 55,
+        "favoritos": [
+            {"nombre": "Pastor", "ultima-fecha": "22 de agosto 2024"} 
+        ]
+    }
+}
+
+items = {
+    "ajsdh": {
+        "nombre": "Televisión",
+        "precio": 29900.90
+    }
+}
