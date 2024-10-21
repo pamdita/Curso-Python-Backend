660f9e3079a54d182f307164f84f660fbc1e1237
Daniel <d.a.p.mendez@outlook.com>
2024-10-20 23:05:33 -0600

    base de datos

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from super_db import usuarios, items
 
 app = FastAPI()
 
 @app.get("/")
 def get_base():
     return("Mensaje": "Hola Mundo")
+
+@app.get("/usuario/{id}")
+def get_usuario(id: int)
+    
